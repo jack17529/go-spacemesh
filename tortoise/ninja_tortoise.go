9c237fba4314258dfde4fdaff4c9e09686f5be73
rehs0y <lyosher@gmail.com>
2020-07-08 20:29:43 +0300

    add more tests and fix some bugs.

--- tortoise/ninja_tortoise.go
+++ tortoise/ninja_tortoise.go
@@ -13,46 +13,6 @@ import (
 	"github.com/spacemeshos/go-spacemesh/mesh"
 )
 
-type vec [2]int
-type patternID uint32 //this hash does not include the layer id
-
-const ( //Threshold
-	window          = 10
-	globalThreshold = 0.6
-	genesis         = 0
-)
-
-var ( //correction vectors type
-	//Opinion
-	support     = vec{1, 0}
-	against     = vec{0, 1}
-	abstain     = vec{0, 0}
-	zeroPattern = votingPattern{}
-)
-
-func max(i types.LayerID, j types.LayerID) types.LayerID {
-	if i > j {
-		return i
-	}
-	return j
-}
-
-func (a vec) Add(v vec) vec {
-	return vec{a[0] + v[0], a[1] + v[1]}
-}
-
-func (a vec) Negate() vec {
-	a[0] = a[0] * -1
-	a[1] = a[1] * -1
-	return a
-}
-
-func (a vec) Multiply(x int) vec {
-	a[0] = a[0] * x
-	a[1] = a[1] * x
-	return a
-}
-
 type blockIDLayerTuple struct {
 	types.BlockID
 	types.LayerID
